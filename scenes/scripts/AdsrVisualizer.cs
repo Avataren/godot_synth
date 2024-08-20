4e81da9bb9ab37e8c91fde5f0010d250c1e79197
Morten Andersson <morten.andersson@gmail.com>
2024-08-20 18:55:09 +0200

    merge fix

--- scenes/scripts/AdsrVisualizer.cs
+++ scenes/scripts/AdsrVisualizer.cs
@@ -13,10 +13,13 @@ public partial class AdsrVisualizer : PanelContainer
 	EnvelopeNode[] envelopeNodes = new EnvelopeNode[MaxEnvelopes];
 	float[] visualBuffer = new float[512];
 	ShaderMaterial node_shader_material;
+
+	float TimeScale = 1.0f;
 	public override void _Ready()
 	{
 		node_shader_material = (ShaderMaterial)ShaderRect.Material;
 		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512);
+		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512, TimeScale * 3.0f);
 		// for (int i = 0; i < visualBuffer.Length; i++)
 		// {
 		// 	GD.Print("visualBuffer[" + i + "] = " + visualBuffer[i]);
@@ -88,32 +91,67 @@ public partial class AdsrVisualizer : PanelContainer
 		GD.Print("Setting node reference for index " + index + " to " + node.Name);
 		envelopeNodes[index] = node;
 	}
+	private void _on_time_knob_value_changed(float val)
+	{
+		TimeScale = val;
+		//envelopeNode.TimeScale = val;
+		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512, 3.0f);
+		node_shader_material.SetShaderParameter("wave_data", visualBuffer);
+		node_shader_material.SetShaderParameter("total_time", TimeScale* 3.0f);
+	}
+
+	private void _on_attack_c_knob_value_changed(float val)
+	{
+		if (Mathf.Abs(val) < 0.0015)
+			val = 0.0015f;
+		currentEnvelopeNode.AttackCtrl = val;
+		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512, 3.0f);
+		node_shader_material.SetShaderParameter("wave_data", visualBuffer);
+	}
+
+	private void _on_decay_c_knob_value_changed(float val)
+	{
+		if (Mathf.Abs(val) < 0.0015)
+			val = 0.0015f;
+		currentEnvelopeNode.DecayCtrl = val;
+		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512,  3.0f);
+		node_shader_material.SetShaderParameter("wave_data", visualBuffer);
+	}
+
+	private void _on_release_c_knob_value_changed(float val)
+	{
+		if (Mathf.Abs(val) < 0.0015)
+			val = 0.0015f;
+		currentEnvelopeNode.ReleaseCtrl = val;
+		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512,  3.0f);
+		node_shader_material.SetShaderParameter("wave_data", visualBuffer);
+	}
 
 	private void _on_attack_knob_value_changed(float val)
 	{
 		currentEnvelopeNode.AttackTime = val;
-		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512);
+		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512,  3.0f);
 		node_shader_material.SetShaderParameter("wave_data", visualBuffer);
 	}
 
 	private void _on_decay_knob_value_changed(float val)
 	{
 		currentEnvelopeNode.DecayTime = val;
-		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512);
+		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512, 3.0f);
 		node_shader_material.SetShaderParameter("wave_data", visualBuffer);
 	}
 
 	private void _on_sustain_knob_value_changed(float val)
 	{
 		currentEnvelopeNode.SustainLevel = val;
-		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512);
+		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512,  3.0f);
 		node_shader_material.SetShaderParameter("wave_data", visualBuffer);
 	}
 
 	private void _on_release_knob_value_changed(float val)
 	{
 		currentEnvelopeNode.ReleaseTime = val;
-		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512);
+		visualBuffer = currentEnvelopeNode.GetVisualBuffer(512, 3.0f);
 		node_shader_material.SetShaderParameter("wave_data", visualBuffer);
 	}
 

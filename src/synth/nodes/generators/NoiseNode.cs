4f514cd74893b44da7528a1edc12d9e24e6f1264
Morten Andersson <morten.andersson@gmail.com>
2024-08-27 19:19:05 +0200

    fixed pink noise

--- src/synth/nodes/generators/NoiseNode.cs
+++ src/synth/nodes/generators/NoiseNode.cs
@@ -71,7 +71,7 @@ namespace Synth
 
             float minFrequency = 20f;
             float maxFrequency = SampleRate / 2f;
-            
+
             float cutoffFrequency;
             if (currentFrequencySlope < 0)
             {
@@ -113,19 +113,20 @@ namespace Synth
             return new Vector<float>(noiseValues);
         }
 
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private float GetPinkNoise()
         {
-            float white = GetWhiteNoise();
-            pinkNoiseState[0] = 0.99886f * pinkNoiseState[0] + white * 0.0555179f;
-            pinkNoiseState[1] = 0.99332f * pinkNoiseState[1] + white * 0.0750759f;
-            pinkNoiseState[2] = 0.96900f * pinkNoiseState[2] + white * 0.1538520f;
-            pinkNoiseState[3] = 0.86650f * pinkNoiseState[3] + white * 0.3104856f;
-            pinkNoiseState[4] = 0.55000f * pinkNoiseState[4] + white * 0.5329522f;
-            pinkNoiseState[5] = -0.7616f * pinkNoiseState[5] - white * 0.0168980f;
-            float pink = pinkNoiseState[0] + pinkNoiseState[1] + pinkNoiseState[2] + pinkNoiseState[3] + pinkNoiseState[4] + pinkNoiseState[5] + pinkNoiseState[6] + white * 0.5362f;
-            pinkNoiseState[6] = white * 0.115926f;
-            return pink * 0.11f;
+            float pink = 0f;
+            pinkNoiseState[0] = 0.99886f * pinkNoiseState[0] + GetWhiteNoise() * 0.0555179f;
+            pinkNoiseState[1] = 0.99332f * pinkNoiseState[1] + GetWhiteNoise() * 0.0750759f;
+            pinkNoiseState[2] = 0.96900f * pinkNoiseState[2] + GetWhiteNoise() * 0.1538520f;
+            pinkNoiseState[3] = 0.86650f * pinkNoiseState[3] + GetWhiteNoise() * 0.3104856f;
+            pinkNoiseState[4] = 0.55000f * pinkNoiseState[4] + GetWhiteNoise() * 0.5329522f;
+            pinkNoiseState[5] = -0.7616f * pinkNoiseState[5] - GetWhiteNoise() * 0.0168980f;
+            pink = pinkNoiseState[0] + pinkNoiseState[1] + pinkNoiseState[2] + pinkNoiseState[3] + pinkNoiseState[4] + pinkNoiseState[5] + pinkNoiseState[6] + GetWhiteNoise() * 0.5362f;
+            pinkNoiseState[6] = GetWhiteNoise() * 0.115926f;
+            return pink * 0.11f; // Scale to roughly the same range as white noise
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]

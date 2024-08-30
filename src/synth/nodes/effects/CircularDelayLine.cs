8a638e13dfd03dd8165da0689cf31b1be08d2bac
Morten Andersson <morten.andersson@gmail.com>
2024-08-30 14:08:24 +0200

    use all pass interpolation instead of cubic

--- src/synth/nodes/effects/CircularDelayLine.cs
+++ src/synth/nodes/effects/CircularDelayLine.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Runtime.CompilerServices;
 
@@ -11,12 +10,14 @@ namespace Synth
         private readonly int bufferSize;
         private int readIndex;
         private SynthType fraction;
+        private SynthType prevOutput;
 
         public CircularDelayLine(int sizeInSamples)
         {
             bufferSize = sizeInSamples;
             buffer = new SynthType[bufferSize];
             Array.Clear(buffer, 0, bufferSize);
+            prevOutput = 0;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -30,25 +31,18 @@ namespace Synth
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public SynthType GetSample(SynthType inputSample)
         {
-            // Cubic interpolation
-            int index0 = (readIndex - 1 + bufferSize) % bufferSize;
-            int index1 = readIndex;
-            int index2 = (readIndex + 1) % bufferSize;
-            int index3 = (readIndex + 2) % bufferSize;
+            // All-pass interpolation
+            int index0 = readIndex;
+            int index1 = (readIndex + 1) % bufferSize;
 
-            SynthType y0 = buffer[index0];
-            SynthType y1 = buffer[index1];
-            SynthType y2 = buffer[index2];
-            SynthType y3 = buffer[index3];
+            SynthType frac = fraction;
+            SynthType a = (1 - frac) / (1 + frac);
 
-            SynthType mu = fraction;
-            SynthType mu2 = mu * mu;
-            SynthType a0 = y3 - y2 - y0 + y1;
-            SynthType a1 = y0 - y1 - a0;
-            SynthType a2 = y2 - y0;
-            SynthType a3 = y1;
+            SynthType x0 = buffer[index0];
+            SynthType x1 = buffer[index1];
 
-            SynthType outputSample = a0 * mu * mu2 + a1 * mu2 + a2 * mu + a3;
+            SynthType outputSample = a * (x0 - prevOutput) + x1;
+            prevOutput = outputSample;
 
             buffer[writeIndex] = inputSample;
             writeIndex = (writeIndex + 1) % bufferSize;
@@ -59,7 +53,73 @@ namespace Synth
         public void Mute()
         {
             Array.Clear(buffer, 0, bufferSize);
+            prevOutput = 0;
         }
     }
-
 }
+
+
+// using System;
+// using System.Runtime.CompilerServices;
+
+// namespace Synth
+// {
+//     public class CircularDelayLine
+//     {
+//         private readonly SynthType[] buffer;
+//         private int writeIndex;
+//         private readonly int bufferSize;
+//         private int readIndex;
+//         private SynthType fraction;
+
+//         public CircularDelayLine(int sizeInSamples)
+//         {
+//             bufferSize = sizeInSamples;
+//             buffer = new SynthType[bufferSize];
+//             Array.Clear(buffer, 0, bufferSize);
+//         }
+
+//         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+//         public void SetDelayInSamples(SynthType delaySamples)
+//         {
+//             int intDelay = (int)delaySamples;
+//             fraction = delaySamples - intDelay;
+//             readIndex = (writeIndex - intDelay + bufferSize) % bufferSize;
+//         }
+
+//         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+//         public SynthType GetSample(SynthType inputSample)
+//         {
+//             // Cubic interpolation
+//             int index0 = (readIndex - 1 + bufferSize) % bufferSize;
+//             int index1 = readIndex;
+//             int index2 = (readIndex + 1) % bufferSize;
+//             int index3 = (readIndex + 2) % bufferSize;
+
+//             SynthType y0 = buffer[index0];
+//             SynthType y1 = buffer[index1];
+//             SynthType y2 = buffer[index2];
+//             SynthType y3 = buffer[index3];
+
+//             SynthType mu = fraction;
+//             SynthType mu2 = mu * mu;
+//             SynthType a0 = y3 - y2 - y0 + y1;
+//             SynthType a1 = y0 - y1 - a0;
+//             SynthType a2 = y2 - y0;
+//             SynthType a3 = y1;
+
+//             SynthType outputSample = a0 * mu * mu2 + a1 * mu2 + a2 * mu + a3;
+
+//             buffer[writeIndex] = inputSample;
+//             writeIndex = (writeIndex + 1) % bufferSize;
+//             readIndex = (readIndex + 1) % bufferSize;
+//             return outputSample;
+//         }
+
+//         public void Mute()
+//         {
+//             Array.Clear(buffer, 0, bufferSize);
+//         }
+//     }
+
+// }
